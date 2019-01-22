06b8c20102e32588438c7e8a53fbd5c3198fa298
Eric Johnston <ej@machinelevel.com>
2019-01-22 10:30:04 -0500

    [shortcuts] Shorter shortcuts

--- sample_shortcuts.js
+++ sample_shortcuts.js
@@ -1,5 +1,5 @@
 // This is a list of the shortcut links provided to shorten book sample URLs
 
 var sample_shortcuts = {
-    teleport1 : 'https://oreilly-qc.github.io/samples/QCEngine/ch04_basic_teleportation.js',
+    teleport1 : 'samples/QCEngine/ch04_basic_teleportation.js',
 };
