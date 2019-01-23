4fbb5e12bfbe593fd2958ca85d51574cacc524aa
Eric Johnston <ej@machinelevel.com>
2019-01-23 09:29:16 -0500

    [fetch test] fetch test

--- sample_shortcuts.js
+++ sample_shortcuts.js
@@ -1,5 +1,17 @@
 // This is a list of the shortcut links provided to shorten book sample URLs
 
-var sample_shortcuts = {
-    teleport1 : 'samples/QCEngine/ch04_basic_teleportation.js',
-};
+// var sample_shortcuts = {
+//     teleport1 : 'samples/QCEngine/ch04_basic_teleportation.js',
+// };
+
+var engine_list = [
+    {name : 'QCEngine', suffix : '.js',  subdir : 'samples/QCEngine/'},
+    {name : 'Qiskit',   suffix : '.py',  subdir : 'samples/Qiskit/'},
+    {name : 'QASM',     suffix : '.asm', subdir : 'samples/QASM/'},
+];
+
+var sample_menu = [
+    {shortcut: 'teleport1', menu_title : 'Ch4 Basic Teleportation',     sample_file : 'ch04_basic_teleportation'},
+    {shortcut: 'fly',       menu_title : 'Ch4 A Fly in the Teleporter', sample_file : 'ch04_teleport_fly'},
+];
+
