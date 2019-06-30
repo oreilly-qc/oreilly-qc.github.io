9615a586a35dde7b54c9bc93a8391f8f1e1e9a0c
ej <ej@machinelevel.com>
2019-06-30 08:07:11 -0400

    [minor] Fixed a typo and updated a web link

--- sample_shortcuts.js
+++ sample_shortcuts.js
@@ -68,7 +68,7 @@ var sample_menu = [
     {shortcut:'11-5', menu_title:'Ex 11-5: Build QSS Lookup Table',        sample_file: 'ch11_circles1', num_qubits: 8, num_instructions: 6, num_circle_cols: 8, num_circle_rows: 1, circle_scale: 1.0, gate_scale: 1.0},
     {shortcut:'11-6', menu_title:'Ex 11-6: Adding Color',           sample_file: 'ch11_circles2', num_qubits: 8, num_instructions: 6, num_circle_cols: 8, num_circle_rows: 1, circle_scale: 1.0, gate_scale: 1.0},
 
-    {shortcut:'12-1', menu_title:'Ex 12-1: Shor\'s Factroring Algorithm', sample_file: 'ch12_shor1',          num_qubits: 12, num_instructions: 6, num_circle_cols: 8, num_circle_rows: 1, circle_scale: 1.0, gate_scale: 1.0},
+    {shortcut:'12-1', menu_title:'Ex 12-1: Shor\'s Factoring Algorithm', sample_file: 'ch12_shor1',          num_qubits: 12, num_instructions: 6, num_circle_cols: 8, num_circle_rows: 1, circle_scale: 1.0, gate_scale: 1.0},
     {shortcut:'12-2', menu_title:'Ex 12-2: Shor without a QPU',           sample_file: 'ch12_shor_no_qpu',    num_qubits: 12, num_instructions: 6, num_circle_cols: 8, num_circle_rows: 1, circle_scale: 1.0, gate_scale: 1.0},
     {shortcut:'12-3', menu_title:'Ex 12-3: Shor step-by-step',           sample_file: 'ch12_shor_complete',    num_qubits: 12, num_instructions: 6, num_circle_cols: 8, num_circle_rows: 1, circle_scale: 1.0, gate_scale: 1.0},
     {shortcut:'12-4', menu_title:'Ex 12-4: Shor classical followup',      sample_file: 'ch12_shor_spike_est', num_qubits: 12, num_instructions: 6, num_circle_cols: 8, num_circle_rows: 1, circle_scale: 1.0, gate_scale: 1.0},
