271117694df1448e15aa91de65252324ec528ade
ej <ej@machinelevel.com>
2019-07-02 07:46:25 -0400

    [GT sample WIP] This sample is under construction

--- samples/QCEngine/ch14_gate_teleportation.js
+++ samples/QCEngine/ch14_gate_teleportation.js
@@ -3,6 +3,67 @@
 //   O'Reilly Media
 
 // To run this online, go to http://oreilly-qc.github.io?p=14-GT
-// Note: This sample may vary slightly from the text in the book,
-// due to revisions or aesthetic tweaks.
 
+//////////////////////////////////////////////
+//////////////////////////////////////////////
+//////////////////////////////////////////////
+//////////////////////////////////////////////
+// NOTE: This sample is undergoing revision.
+//       (almost done)
+//////////////////////////////////////////////
+//////////////////////////////////////////////
+//////////////////////////////////////////////
+//////////////////////////////////////////////
+
+qc.reset(3);
+var alice = qint.new(1, 'alice');
+var ep    = qint.new(1, 'ep');
+var bob   = qint.new(1, 'bob');
+
+ep.write(0);
+bob.write(0);
+qc.label('entangle');
+ep.had();
+bob.cnot(ep);
+qc.label('');
+
+alice.write(0);
+qc.label('prep payload');
+alice.had();
+alice.phase(45);
+alice.had();
+qc.label('');
+qc.nop();
+qc.print('alice prob:' + alice.peekProbability(1));
+
+qc.label('send');
+ep.cnot(alice);
+alice.had();
+alice.read();
+ep.read();
+qc.label('');
+qc.nop();
+
+// ej TODO: Check code order issue
+qc.label('apply gate');
+bob.had();
+//bob.phase(30);
+bob.had();
+qc.label('');
+
+qc.label('receive');
+bob.cnot(ep);
+bob.cz(alice);
+
+qc.label('');
+qc.nop();
+
+
+qc.label('verify');
+bob.had();
+//bob.phase(-45-30);
+bob.phase(-45);
+bob.had();
+bob.read();
+qc.label('');
+qc.nop();
