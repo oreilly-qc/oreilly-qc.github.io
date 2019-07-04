7b95b96e563e1d53afdabb16348e7ccf6dfcaadd
ej <ej@machinelevel.com>
2019-07-04 00:46:02 -0400

    [12-4] Sample 12-4 done

--- samples/QCEngine/ch12_shor_spike_est.js
+++ samples/QCEngine/ch12_shor_spike_est.js
@@ -3,6 +3,16 @@
 //   O'Reilly Media
 
 // To run this online, go to http://oreilly-qc.github.io?p=12-4
+// Note: This sample may vary slightly from the text in the book,
+// due to revisions or aesthetic tweaks.
+
+
+var spike_position = 8;
+var data_range = 16;
+
+var num_spikes = estimate_num_spikes(spike_position, data_range);
+
+qc.print('Estimated number of spikes: ' + num_spikes + '\n');
 
 function estimate_num_spikes(spike, range)
 {
